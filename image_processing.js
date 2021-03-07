1fbba25785637a8c33520937c4380b59bd3b2138
S0ph1ia <badalovasophia@gmail.com>
2021-03-07 20:07:57 +0000

    Changed image_processing.js, to handle only resize and return the image, not writing to anything.

--- image_processing.js
+++ image_processing.js
@@ -1,16 +1,9 @@
 const sharp = require("sharp");
 
-
 function image_processing() {
-     sharp.cache("false")
-    sharp('image.jpg').resize({height: 1000, width: 1024})
-    .then( function(newFileInfo){
-        return "image processed Successfully";
-    })
-    .catch( function (err) {
-        console.log(err);
-        return "Error received";
-    })
+    const image = sharp('image.jpg');
+    image.resize({height: 1000, width: 1024});
+    return image;
 }
 
 module.exports = {
